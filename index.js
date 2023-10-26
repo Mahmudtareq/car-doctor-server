837efa21587186e49ca63353272861a1562884a3
Tareq Mahmud <tareqlpu5@gmail.com>
2023-10-27 02:19:42 +0600

    cretae booking api , get , post

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const cors = require("cors");
+const { connectToDatabase, client } = require("./dataBase");
+const { ObjectId } = require("mongodb");
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// route
+app.get("/", (req, res) => {
+  res.send("card doctor is running");
+});
+// database
+const run = async () => {
+  try {
+    //   database connection
+    await connectToDatabase();
+    // create collection
+    const database = client.db("carDoctorDB");
+    const serviceCollection = database.collection("services");
+    const bookingsCollection = database.collection("bookings");
+    // get all services
+    app.get("/services", async (req, res) => {
+      const allServices = serviceCollection.find();
+      const result = await allServices.toArray();
+      res.send(result);
+    });
+    // single service
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    });
+    // get booking by enail
+    app.get("/bookings", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await bookingsCollection.find(query).toArray();
+      res.send(result);
+    });
+    // booking api
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      const bookings = await bookingsCollection.insertOne(booking);
+      res.send(bookings);
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+run();
+
+app.listen(port, () => {
+  console.log(`Car doctor app listening on port ${port}`);
+});
